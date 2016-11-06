d4e247a0cdc498a4e2c5f1ed141799d94baabfa5
lemanahuel <lema.nahuel@gmail.com>
2016-11-06 09:34:46 -0300

    IMPROVE, endpoint messages

--- server/controllers/public/messages.js
+++ server/controllers/public/messages.js
@@ -1,30 +1,36 @@
 const helpers = require('../../helpers'),
+  async = require('async'),
   _ = require('lodash');
 const FCM = require('fcm-push');
 const fcm = new FCM('AIzaSyB6GGfmBH5AB9lkXnUD96cpci6JpwnKLb0');
+let users = [];
 
 module.exports = class Messages {
 
   static send(req, res, next) {
-    console.log(req.body);
-    console.log('token', req.body.token);
-    console.log('token', req.body.token.toString());
+    users.push(req.body);
 
     //collapse_key: 'your_collapse_key',
     // data: {
     //   your_custom_data_key: 'your_custom_data_value'
     // },
 
-    fcm.send({
-      to: req.body.token.toString(),
-      notification: {
-        title: 'Nueva Emergencia',
-        body: 'Hola ' + req.body.username
-      }
-    }).then((res) => {
-      console.log("Successfully sent with response: ", res);
-    }).catch((err) => {
-      console.log("Something has gone wrong!", err);
+    async.each(users, (user, cb) => {
+      fcm.send({
+        to: user.token,
+        notification: {
+          title: 'Nueva Emergencia',
+          body: 'Hola ' + user.username
+        }
+      }).then((res) => {
+        console.log("Successfully sent with response: ", res);
+        cb(null);
+      }).catch((err) => {
+        console.log("Something has gone wrong!", err);
+        cb(null);
+      });
+    }, (err) => {
+      console.log('FINISH-USERS', users.length);
     });
   }
 
