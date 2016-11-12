9322065c67b6f05d17c592a79e15fb654b952dfd
lemanahuel <lema.nahuel@gmail.com>
2016-11-12 14:03:01 -0300

    HOT FIX, endpoint de capacitation centers

--- server/routes/public/capacitationCenters.js
+++ server/routes/public/capacitationCenters.js
@@ -4,7 +4,7 @@ const CC = require('../../controllers/public/capacitationCenters');
 
 module.exports = (app) => {
 
-  app.route('/capacitation-centers').get(CC.read);
+  app.route('/capacitation-centers').get(CC.list);
   app.route('/capacitation-centers/:ccSlug').get(CC.readBySlug);
 
 };
